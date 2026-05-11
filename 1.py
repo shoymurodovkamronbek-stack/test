24af7a41dad6f456fd5f7a3fcdde3d84543b1f52
brighttpath2011-ux <brightpath2011@gmail.com>
2026-05-11 18:59:35 +0500

    yangliandi

---
+++ 1.py
@@ -0,0 +1,11 @@
+import os 
+os.system("cls")
+
+with open ("N205/image.jpeg", "rb") as file :
+    baytlar = file.read () 
+    print (f"\n{len(baytlar)} ta raqam ")
+
+
+with open ("rasm.jpeg" , "wb" ) as file : 
+    file.write(baytlar) 
+    print ("fayl nusxalandie ")   
